7d2d86cac9edb9b9717f367833e1466f96ae48f6
prempj2022 <premrajyuvi@gmail.com>
2022-09-24 21:40:50 +0530

    newcode

---
+++ src/org/favcric/Cricket.java
@@ -0,0 +1,18 @@
+package org.favcric;
+
+public class Cricket {
+	private void myfav(){
+		System.out.println("yuvi");
+		}
+    private void jerseynum() {
+    	System.out.println("12");
+    }
+    public static void main(String[] args) {
+		//objectcreation
+		Cricket j= new Cricket();
+		//method calling
+		j.myfav();
+		j.jerseynum();
+    }
+
+}
